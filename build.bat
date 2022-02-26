b7813170934155d72ea80d53a51769e0f6940492
Boyd <matthbo@amazon.com>
2022-02-26 12:32:12 -0500

    Added build.bat for Windows builds

--- build.bat
+++ build.bat
@@ -2,7 +2,6 @@ python -m pip install pipenv
 export PIPENV_VENV_IN_PROJECT="enabled"
 pipenv install
 pipenv run pyinstaller aws-transcript.py --onefile
-cp dist/aws-transcript .
 pipenv --rm
-rm -r build
-rm -r dist
+rmdir /s /q build
+REM rmdir /s /q dist
