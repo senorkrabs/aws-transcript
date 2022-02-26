a50468b23d948fdd1b1fb5fbb923fd215071d436
Boyd <matthbo@amazon.com>
2022-02-26 12:41:16 -0500

    Updated pipfile and batch script

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 python -m pip install pipenv
-export PIPENV_VENV_IN_PROJECT="enabled"
+SET PIPENV_VENV_IN_PROJECT="enabled"
 pipenv install
 pipenv run pyinstaller aws-transcript.py --onefile
 pipenv --rm
